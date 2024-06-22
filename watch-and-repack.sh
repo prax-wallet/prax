429f41ea6a298f9866643a3752b06466ba9ad7fc
Tal Derei <talderei99@gmail.com>
2024-06-21 23:29:16 -0700

    remove existing tarballs

--- watch-and-repack.sh
+++ watch-and-repack.sh
@@ -7,6 +7,11 @@ PRAX_REPO_PATH="."
 # Ensure 'pack-public.sh' is executable
 chmod +x "$PENUMBRA_ZONE_WEB_PATH/pack-public.sh"
 
+# Remove any existing tarballs from packages in penumbra-zone/web 
+cleanup_tarballs() {
+  find "$PENUMBRA_ZONE_WEB_PATH/packages" -name "*.tgz" -type f -delete
+}
+
 # Repack the packages in penumbra-zone/web
 repack() {
   (cd "$PENUMBRA_ZONE_WEB_PATH" && ./pack-public.sh)
@@ -26,6 +31,7 @@ reload_webpack() {
 # Watch for changes in penumbra-zone/web and trigger repack and reload
 while true; do
   fswatch -1 -o "$PENUMBRA_ZONE_WEB_PATH/packages" | while read -r; do
+    cleanup_tarballs
     repack
     install_prax
     reload_webpack
