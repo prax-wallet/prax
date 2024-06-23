8f850b82a3383b315a35cb2e479775b73c4b9f06
Tal Derei <talderei99@gmail.com>
2024-06-22 18:08:15 -0700

    suggestions

--- watch-and-repack.sh
+++ watch-and-repack.sh
@@ -4,14 +4,6 @@
 PENUMBRA_ZONE_WEB_PATH="../../web"
 PRAX_REPO_PATH="."
 
-# Ensure 'pack-public.sh' is executable
-chmod +x "$PENUMBRA_ZONE_WEB_PATH/pack-public.sh"
-
-# Remove any existing tarballs from packages in penumbra-zone/web 
-cleanup_tarballs() {
-  find "$PENUMBRA_ZONE_WEB_PATH/packages" -name "*.tgz" -type f -delete
-}
-
 # Repack the packages in penumbra-zone/web
 repack() {
   (cd "$PENUMBRA_ZONE_WEB_PATH" && ./pack-public.sh)
@@ -24,14 +16,13 @@ install_prax() {
 
 # Reload webpack
 reload_webpack() {
-  (cd "$PRAX_REPO_PATH" && pkill -f "webpack-dev-server")
+  (cd "$PRAX_REPO_PATH" && pkill -f "webpack-prax-dev-server")
   (cd "$PRAX_REPO_PATH" && pnpm run dev &)
 }
 
 # Watch for changes in penumbra-zone/web and trigger repack and reload
 while true; do
   fswatch -1 -o "$PENUMBRA_ZONE_WEB_PATH/packages" | while read -r; do
-    cleanup_tarballs
     repack
     install_prax
     reload_webpack
