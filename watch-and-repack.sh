3f2925fa4fca02caa5fd6b0fa4b6293f53ac18a7
Tal Derei <talderei99@gmail.com>
2024-06-22 18:25:51 -0700

    more suggestions

--- watch-and-repack.sh
+++ watch-and-repack.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Relative paths to the web repositories
-PENUMBRA_ZONE_WEB_PATH="../../web"
-PRAX_REPO_PATH="."
+# Check if `PENUMBRA_ZONE_WEB_PATH` is set and points to a valid directory
+if [ -z "$PENUMBRA_ZONE_WEB_PATH" ] || [ ! -d "$PENUMBRA_ZONE_WEB_PATH" ]; then
+  PENUMBRA_ZONE_WEB_PATH="../../web"
+fi
 
-# Repack the packages in penumbra-zone/web
+# Check if `PRAX_REPO_PATH` is set and points to a valid directory
+if [ -z "$PRAX_REPO_PATH" ] || [ ! -d "$PRAX_REPO_PATH" ]; then
+  PRAX_REPO_PATH="."
+fi
+
+# Repack the packages in `penumbra-zone/web`
 repack() {
   (cd "$PENUMBRA_ZONE_WEB_PATH" && ./pack-public.sh)
 }
 
-# Install dependencies in prax-wallet/web
+# Install dependencies in `prax-wallet/web`
 install_prax() {
-  (cd "$PRAX_REPO_PATH" && pnpm add -w ../../web/packages/*/penumbra-zone-*-*.tgz)
+  (cd "$PRAX_REPO_PATH" && pnpm add -w $PENUMBRA_ZONE_WEB_PATH/packages/*/penumbra-zone-*-*.tgz)
 }
 
 # Reload webpack
@@ -20,7 +26,7 @@ reload_webpack() {
   (cd "$PRAX_REPO_PATH" && pnpm run dev &)
 }
 
-# Watch for changes in penumbra-zone/web and trigger repack and reload
+# Watch for changes in `penumbra-zone/web` and trigger repack and reload
 while true; do
   fswatch -1 -o "$PENUMBRA_ZONE_WEB_PATH/packages" | while read -r; do
     repack
