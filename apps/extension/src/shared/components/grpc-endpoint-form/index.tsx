bdb5eda9725596e00f3533890dc31e85aa42508c
Atris <vacekj@outlook.com>
2024-06-25 20:20:56 +0200

    fix: trim trailing slash from custom rpc url

--- apps/extension/src/shared/components/grpc-endpoint-form/index.tsx
+++ apps/extension/src/shared/components/grpc-endpoint-form/index.tsx
@@ -76,7 +76,7 @@ export const GrpcEndpointForm = ({
                   placeholder='Enter URL'
                   ref={customGrpcEndpointInput}
                   value={isCustomGrpcEndpoint && !!grpcEndpointInput ? grpcEndpointInput : ''}
-                  onChange={e => setGrpcEndpointInput(e.target.value)}
+                  onChange={e => setGrpcEndpointInput(e.target.value.replace(/\/$/, ''))}
                   className='w-full rounded border border-secondary bg-background p-1 outline-0 transition hover:border-gray-400 focus:border-gray-400'
                 />
               }
