3ac5711f28e06b3c0ebb79efa0123675b8c1ade2
Gabe Rodriguez <grod220@gmail.com>
2024-06-26 12:06:55 +0200

    Guard against bad default frontends

--- apps/extension/src/shared/utils/is-valid-url.ts
+++ apps/extension/src/shared/utils/is-valid-url.ts
@@ -1,6 +1,13 @@
 export const isValidUrl = (url: string) => {
   try {
-    new URL(url);
+    const proposedUrl = new URL(url);
+
+    // Security measure: allows us to guard against someone being deceived
+    // into adding a url with chrome-extension:// in it with intentions to inject code
+    if (!['https:', 'http:'].includes(proposedUrl.protocol)) {
+      throw new Error(`protocol "${proposedUrl.protocol}" is not supported`);
+    }
+
     return true;
   } catch {
     return false;
