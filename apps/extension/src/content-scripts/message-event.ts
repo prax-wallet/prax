4da58add3df0a0594dbc990db6ae6c5380de136f
turbocrime <turbocrime@users.noreply.github.com>
2024-06-26 18:53:09 -0700

    use unwrap in type guards

--- apps/extension/src/content-scripts/message-event.ts
+++ apps/extension/src/content-scripts/message-event.ts
@@ -6,36 +6,31 @@ import { PraxConnection } from '../message/prax';
 export type PraxMessage<T = unknown> = { [PRAX]: T };
 export type PraxMessageEvent<T = unknown> = MessageEvent<PraxMessage<T>>;
 
+export const unwrapPraxMessageEvent = <T>(ev: PraxMessageEvent<T>): T =>
+  // @ts-expect-error - ts can't understand the injected string
+  ev.data[PRAX] as T;
+
 export const isPraxMessageEvent = (ev: MessageEvent<unknown>): ev is PraxMessageEvent =>
   typeof ev.data === 'object' && ev.data !== null && PRAX in ev.data;
 
 export const isPraxPortMessageEvent = (
   ev: MessageEvent<unknown>,
 ): ev is PraxMessageEvent<MessagePort> =>
-  // @ts-expect-error - ts can't understand the injected string
-  isPraxMessageEvent(ev) && ev.data[PRAX] instanceof MessagePort;
+  isPraxMessageEvent(ev) && unwrapPraxMessageEvent(ev) instanceof MessagePort;
 
 export const isPraxRequestMessageEvent = (
   ev: MessageEvent<unknown>,
 ): ev is PraxMessageEvent<PraxConnection.Request> =>
-  // @ts-expect-error - ts can't understand the injected string
-  isPraxMessageEvent(ev) && ev.data[PRAX] === PraxConnection.Request;
+  isPraxMessageEvent(ev) && unwrapPraxMessageEvent(ev) === PraxConnection.Request;
 
 export const isPraxEndMessageEvent = (
   ev: MessageEvent<unknown>,
 ): ev is PraxMessageEvent<PraxConnection.Disconnect> =>
-  // @ts-expect-error - ts can't understand the injected string
-  isPraxMessageEvent(ev) && ev.data[PRAX] === PraxConnection.Disconnect;
+  isPraxMessageEvent(ev) && unwrapPraxMessageEvent(ev) === PraxConnection.Disconnect;
 
 export const isPraxFailureMessageEvent = (
   ev: MessageEvent<unknown>,
-): ev is PraxMessageEvent<PenumbraRequestFailure> => {
-  if (!isPraxMessageEvent(ev)) return false;
-  // @ts-expect-error - ts can't understand the injected string
-  const status = ev.data[PRAX] as unknown;
-  return typeof status === 'string' && status in PenumbraRequestFailure;
-};
-
-export const unwrapPraxMessageEvent = <T>(ev: PraxMessageEvent<T>): T =>
-  // @ts-expect-error - ts can't understand the injected string
-  ev.data[PRAX] as T;
+): ev is PraxMessageEvent<PenumbraRequestFailure> =>
+  isPraxMessageEvent(ev) &&
+  typeof unwrapPraxMessageEvent(ev) === 'string' &&
+  unwrapPraxMessageEvent<string>(ev) in PenumbraRequestFailure;
