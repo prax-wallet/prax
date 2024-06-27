4480b313c132fa512dfcba24027167cbb121bbae
Tal Derei <70081547+TalDerei@users.noreply.github.com>
2024-06-27 11:17:23 -0700

    hmr script  (#50)
    * hmr script for prax
    
    * install deps from tarballs
    
    * dynamically reload webpack
    
    * remove existing tarballs
    
    * pm2 process management dep
    
    * lockfile
    
    * kill webpack
    
    * update lock to fix ci
    
    * suggestions
    
    * more suggestions
    
    * use kill for pid
    
    * fail on unset env vars
    
    * fix lockfile
    
    * lock galore

---
+++ watch-and-repack.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Check if `PENUMBRA_ZONE_WEB_PATH` is set and points to a valid directory
+if [ -z "$PENUMBRA_ZONE_WEB_PATH" ] || [ ! -d "$PENUMBRA_ZONE_WEB_PATH" ]; then
+    echo "Error: PENUMBRA_ZONE_WEB_PATH is not set or does not point to a valid directory."
+    exit 1
+fi
+
+# Check if `PRAX_REPO_PATH` is set and points to a valid directory
+if [ -z "$PRAX_REPO_PATH" ] || [ ! -d "$PRAX_REPO_PATH" ]; then
+    echo "Error: PRAX_REPO_PATH is not set or does not point to a valid directory."
+    exit 1
+fi
+
+# Repack the packages in `penumbra-zone/web`
+repack() {
+  (cd "$PENUMBRA_ZONE_WEB_PATH" && ./pack-public.sh)
+}
+
+# Install dependencies in `prax-wallet/web`
+install_prax() {
+  (cd "$PRAX_REPO_PATH" && pnpm add -w $PENUMBRA_ZONE_WEB_PATH/packages/*/penumbra-zone-*-*.tgz)
+}
+
+# Reload webpack
+reload_webpack() {
+  # Find the PID of the actively running webpack process 
+  WEBPACK_PID=$(lsof -t -i:5175) 
+
+  if [ -n "$WEBPACK_PID" ]; then
+    kill -9 $WEBPACK_PID
+  fi
+  
+  (cd "$PRAX_REPO_PATH" && pnpm run dev &)
+}
+
+# Watch for changes in `penumbra-zone/web` and trigger repack and reload
+while true; do
+  fswatch -1 -o "$PENUMBRA_ZONE_WEB_PATH/packages" | while read -r; do
+    repack
+    install_prax
+    reload_webpack
+    break
+  done
+done
