69cb21fad74a2b97cbb82943de7e5dd2c2378f76
turbocrime <turbocrime@users.noreply.github.com>
2024-08-01 11:03:27 -0700

    await txApprovalSelector to catch failures in popup listener

--- apps/extension/src/entry/popup-root.tsx
+++ apps/extension/src/entry/popup-root.tsx
@@ -1,36 +1,47 @@
+import { ConnectError } from '@connectrpc/connect';
+import { errorToJson } from '@connectrpc/connect/protocol-connect';
+import type { InternalResponse } from '@penumbra-zone/types/internal-msg/shared';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import {
+  isOriginApprovalRequest,
+  isPopupRequest,
+  isTxApprovalRequest,
+  OriginApproval,
+  TxApproval,
+} from '../message/popup';
 import { popupRouter } from '../routes/popup/router';
-
-import { isOriginApprovalRequest, isPopupRequest, isTxApprovalRequest } from '../message/popup';
 import { useStore } from '../state';
 import { originApprovalSelector } from '../state/origin-approval';
 import { txApprovalSelector } from '../state/tx-approval';
 
-import { errorToJson } from '@connectrpc/connect/protocol-connect';
-import { ConnectError } from '@connectrpc/connect';
-
 import '@repo/ui/styles/globals.css';
 
 chrome.runtime.onMessage.addListener(
-  (req: unknown, _: chrome.runtime.MessageSender, responder: (x: unknown) => void) => {
+  (
+    req: unknown,
+    _: chrome.runtime.MessageSender,
+    responder: (x: InternalResponse<TxApproval | OriginApproval>) => void,
+  ) => {
     if (isPopupRequest(req)) {
-      try {
-        if (isTxApprovalRequest(req)) {
-          void txApprovalSelector(useStore.getState()).acceptRequest(req, responder);
-        } else if (isOriginApprovalRequest(req)) {
-          originApprovalSelector(useStore.getState()).acceptRequest(req, responder);
-        } else {
-          throw new Error('Unknown popup request');
+      void (async () => {
+        try {
+          if (isTxApprovalRequest(req)) {
+            await txApprovalSelector(useStore.getState()).acceptRequest(req, responder);
+          } else if (isOriginApprovalRequest(req)) {
+            originApprovalSelector(useStore.getState()).acceptRequest(req, responder);
+          } else {
+            throw new Error('Unknown popup request');
+          }
+        } catch (e) {
+          responder({
+            type: req.type,
+            error: errorToJson(ConnectError.from(e), undefined),
+          });
         }
-      } catch (e) {
-        responder({
-          type: req.type,
-          error: errorToJson(ConnectError.from(e), undefined),
-        });
-      }
+      })();
       return true; // instruct chrome runtime to wait for a response
     }
     return false; // instruct chrome runtime we will not respond
