cef99d7f149a84284e0339ad268f39a5cc8338aa
turbocrime <turbocrime@users.noreply.github.com>
2024-08-07 23:57:59 -0700

    rm wrong listener

--- apps/extension/src/content-scripts/injected-connection-port.ts
+++ apps/extension/src/content-scripts/injected-connection-port.ts
@@ -1,23 +1,10 @@
 import { CRSessionClient } from '@penumbra-zone/transport-chrome/session-client';
 import { PraxConnection } from '../message/prax';
-import { isPraxInitMessageEvent, PraxMessage } from './message-event';
+import { PraxMessage } from './message-event';
 
 const end = { [PRAX]: PraxConnection.End } satisfies PraxMessage<PraxConnection.End>;
 
 let port: MessagePort | undefined;
-
-const initListener = (event: MessageEvent<unknown>) => {
-  if (event.origin !== '/') {
-    if (isPraxInitMessageEvent(event)) {
-      window.removeEventListener('message', initListener);
-      if (port) {
-        window.postMessage({ [PRAX]: port } satisfies PraxMessage<MessagePort>, '/', [port]);
-      }
-    }
-  }
-};
-
-window.addEventListener('message', initListener);
 
 chrome.runtime.onMessage.addListener(
   (req: unknown, sender: chrome.runtime.MessageSender, respond: (no?: never) => void) => {
