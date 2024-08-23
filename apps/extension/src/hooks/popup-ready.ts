7e7355b0de1e93318b7606a9365b4f2aa77b8a6c
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-08-23 18:32:40 +0400

    Fix typo

--- apps/extension/src/hooks/popup-ready.ts
+++ apps/extension/src/hooks/popup-ready.ts
@@ -5,13 +5,13 @@ type IsReady = boolean | undefined;
 
 // signals that react is ready (mounted) to service worker
 export const usePopupReady = (isReady: IsReady = undefined) => {
-  const sentMessagedRef = useRef(new Set());
+  const sentMessagesRef = useRef(new Set());
   const searchParams = new URLSearchParams(window.location.search);
   const popupId = searchParams.get('popupId');
 
   useEffect(() => {
-    if (popupId && (isReady === undefined || isReady) && !sentMessagedRef.current.has(popupId)) {
-      sentMessagedRef.current.add(popupId);
+    if (popupId && (isReady === undefined || isReady) && !sentMessagesRef.current.has(popupId)) {
+      sentMessagesRef.current.add(popupId);
 
       void chrome.runtime.sendMessage({
         type: PopupType.Ready,
