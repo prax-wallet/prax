a9d0478e3c5cc72e6eb5136d80c5a49de092deb9
Jason M. Hasperhoven <martijn.hasperhoven@gmail.com>
2024-08-28 13:52:02 +0400

    Remove usePopupReady arg

--- apps/extension/src/hooks/popup-ready.ts
+++ apps/extension/src/hooks/popup-ready.ts
@@ -1,19 +1,17 @@
 import { useEffect, useRef } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-type IsReady = boolean | undefined;
-
 // signals that react is ready (mounted) to service worker
-export const usePopupReady = (isReady: IsReady = undefined) => {
+export const usePopupReady = () => {
   const sentMessagesRef = useRef(new Set());
   const [searchParams] = useSearchParams();
   const popupId = searchParams.get('popupId');
 
   useEffect(() => {
-    if (popupId && (isReady === undefined || isReady) && !sentMessagesRef.current.has(popupId)) {
+    if (popupId && !sentMessagesRef.current.has(popupId)) {
       sentMessagesRef.current.add(popupId);
 
       void chrome.runtime.sendMessage(popupId);
     }
-  }, [popupId, isReady]);
+  }, [popupId]);
 };
