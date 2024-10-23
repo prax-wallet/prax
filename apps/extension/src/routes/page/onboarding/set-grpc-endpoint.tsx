fa6a4b74ddd361c903b9d458ea4c3a1ecf7ccb64
Gabe Rodriguez <grod220@gmail.com>
2024-10-23 11:24:04 +0200

    before hook suggestion

--- apps/extension/src/routes/page/onboarding/set-grpc-endpoint.tsx
+++ apps/extension/src/routes/page/onboarding/set-grpc-endpoint.tsx
@@ -3,6 +3,32 @@ import { FadeTransition } from '@repo/ui/components/ui/fade-transition';
 import { usePageNav } from '../../../utils/navigate';
 import { PagePath } from '../paths';
 import { GrpcEndpointForm } from '../../../shared/components/grpc-endpoint-form';
+import { createPromiseClient } from '@connectrpc/connect';
+import { createGrpcWebTransport } from '@connectrpc/connect-web';
+import { AppService, TendermintProxyService } from '@penumbra-zone/protobuf';
+import { localExtStorage } from '../../../storage/local';
+
+// Because the new seed phrase generation step queries a mainnet rpc,
+// when using a non-mainnet chain id, there is a chance that generated wallet birthday is wrong.
+// This logic fixes this issue after they select their rpc.
+export const correctBirthdayHeightIfNeeded = async (grpcEndpoint: string) => {
+  const transport = createGrpcWebTransport({ baseUrl: grpcEndpoint });
+  const { appParameters } = await createPromiseClient(AppService, transport).appParameters({});
+
+  if (!appParameters?.chainId.includes('penumbra-1')) {
+    const setWalletBirthday = await localExtStorage.get('walletCreationBlockHeight');
+    if (setWalletBirthday) {
+      const tendermintClient = createPromiseClient(TendermintProxyService, transport);
+      const { syncInfo } = await tendermintClient.getStatus({});
+
+      // If the user's birthday is longer than the chain height, that means their mainnet birthday
+      // is too long and needs to be shortened to the current block height of the non-mainnet chain
+      if (syncInfo?.latestBlockHeight && Number(syncInfo.latestBlockHeight) < setWalletBirthday) {
+        await localExtStorage.set('walletCreationBlockHeight', Number(syncInfo.latestBlockHeight));
+      }
+    }
+  }
+};
 
 export const SetGrpcEndpoint = () => {
   const navigate = usePageNav();
@@ -24,7 +50,12 @@ export const SetGrpcEndpoint = () => {
         </CardHeader>
 
         <div className='mt-6'>
-          <GrpcEndpointForm submitButtonLabel='Next' isOnboarding={true} onSuccess={onSuccess} />
+          <GrpcEndpointForm
+            submitButtonLabel='Next'
+            isOnboarding={true}
+            onSuccess={onSuccess}
+            beforeSubmit={correctBirthdayHeightIfNeeded}
+          />
         </div>
       </Card>
     </FadeTransition>
