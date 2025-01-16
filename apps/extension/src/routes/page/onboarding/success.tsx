5de0d111502609bf982f124d097cce3c56358edd
Max Korsunov <maxkors2014@gmail.com>
2025-01-16 10:51:02 +0400

    feat(extension): #244: add DEX link

--- apps/extension/src/routes/page/onboarding/success.tsx
+++ apps/extension/src/routes/page/onboarding/success.tsx
@@ -3,6 +3,8 @@ import { SplashPage } from '@repo/ui/components/ui/splash-page';
 import { useStore } from '../../../state';
 import { getDefaultFrontend } from '../../../state/default-frontend';
 
+const DEX_URL = 'https://dex.penumbra.zone';
+
 export const OnboardingSuccess = () => {
   const defaultFrontendUrl = useStore(getDefaultFrontend);
 
@@ -10,16 +12,29 @@ export const OnboardingSuccess = () => {
     <SplashPage title='Account created'>
       <div className='grid gap-2 text-base font-bold'>
         <p>Connect to a Penumbra frontend to deposit, transfer, stake or swap.</p>
-        <Button
-          variant='gradient'
-          onClick={() => {
-            window.open(defaultFrontendUrl, '_blank');
-            window.close();
-          }}
-          className='mt-4'
-        >
-          Visit web app
-        </Button>
+
+        <div className='flex flex-col gap-2 md:flex-row'>
+          <Button
+            variant='secondary'
+            onClick={() => {
+              window.open(defaultFrontendUrl, '_blank');
+              window.close();
+            }}
+            className='mt-4 grow md:w-1/2'
+          >
+            Visit web app
+          </Button>
+          <Button
+            variant='gradient'
+            onClick={() => {
+              window.open(DEX_URL, '_blank');
+              window.close();
+            }}
+            className='mt-4 grow md:w-1/2'
+          >
+            Visit DEX
+          </Button>
+        </div>
       </div>
     </SplashPage>
   );
