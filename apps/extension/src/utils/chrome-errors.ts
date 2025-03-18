450d8d28ac2a1d93e0c3917fd76760d86167c8bd
turbocrime <turbocrime@users.noreply.github.com>
2025-03-17 18:59:21 -0700

    refactor: content script listeners

---
+++ apps/extension/src/utils/chrome-errors.ts
@@ -0,0 +1,17 @@
+const chromeResponderDroppedMessage =
+  'A listener indicated an asynchronous response by returning true, but the message channel closed before a response was received';
+
+export const isChromeResponderDroppedError = (
+  e: unknown,
+): e is Error & { message: typeof chromeResponderDroppedMessage } =>
+  e instanceof Error &&
+  e.message ===
+    'A listener indicated an asynchronous response by returning true, but the message channel closed before a response was received';
+
+export const suppressChromeResponderDroppedError = (e: unknown) => {
+  if (isChromeResponderDroppedError(e)) {
+    return null;
+  } else {
+    throw e;
+  }
+};
