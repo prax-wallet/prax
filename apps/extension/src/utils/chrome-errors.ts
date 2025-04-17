4837dadf92f8e0d5b36c158166882c1c1a374b3c
turbocrime <134443988+turbocrime@users.noreply.github.com>
2025-04-16 18:18:16 -0700

    don't repeat const string

--- apps/extension/src/utils/chrome-errors.ts
+++ apps/extension/src/utils/chrome-errors.ts
@@ -4,9 +4,7 @@ const chromeResponderDroppedMessage =
 export const isChromeResponderDroppedError = (
   e: unknown,
 ): e is Error & { message: typeof chromeResponderDroppedMessage } =>
-  e instanceof Error &&
-  e.message ===
-    'A listener indicated an asynchronous response by returning true, but the message channel closed before a response was received';
+  e instanceof Error && e.message === chromeResponderDroppedMessage;
 
 export const suppressChromeResponderDroppedError = (e: unknown) => {
   if (isChromeResponderDroppedError(e)) {
