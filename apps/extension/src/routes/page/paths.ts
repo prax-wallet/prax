853def8f61a3b416eadd7490278924dfc3bb796f
Tal Derei <70081547+TalDerei@users.noreply.github.com>
2025-04-30 08:15:35 -0700

    onboarding: remove seedphrase countdown and confirmation page (#335)

--- apps/extension/src/routes/page/paths.ts
+++ apps/extension/src/routes/page/paths.ts
@@ -2,7 +2,6 @@ export enum PagePath {
   INDEX = '/',
   WELCOME = '/welcome',
   GENERATE_SEED_PHRASE = '/welcome/generate',
-  CONFIRM_BACKUP = '/welcome/confirm-backup',
   IMPORT_SEED_PHRASE = '/welcome/import',
   ONBOARDING_SUCCESS = '/welcome/success',
   SET_PASSWORD = '/welcome/set-password',
