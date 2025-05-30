97a9ec01fa7459a7ff7dbee575d2d60b921d0494
Tal Derei <talderei99@gmail.com>
2025-05-30 15:10:04 -0700

    onboarding: redirect to landing page

--- apps/extension/src/routes/page/onboarding/constants.ts
+++ apps/extension/src/routes/page/onboarding/constants.ts
@@ -6,3 +6,6 @@ export const DEFAULT_GRPC = 'https://penumbra-1.radiantcommons.com';
 
 // Define a canconcial default frontend.
 export const DEFAULT_FRONTEND = 'https://dex.penumbra.zone';
+
+// Define a canconcial default landing page.
+export const DEFAULT_LANDING_PAGE = 'https://praxwallet.com/welcome';
