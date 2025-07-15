3a7cfb7051729e45d8b258ffe11ab4fd48264508
the letter L <134443988+turbocrime@users.noreply.github.com>
2025-07-15 11:15:33 -0700

    avoid setting NaN fullSyncHeight (#394)
    * avoid setting NaN fullSyncHeight
    
    * rename file, correct import

--- apps/extension/src/utils/sentinal.ts
+++
@@ -1,4 +0,0 @@
-export const SENTINEL_U64_MAX = BigInt('0xffffffffffffffff');
-
-export const isSentinel = (h: bigint | number | undefined) =>
-  h === SENTINEL_U64_MAX || h === Number(SENTINEL_U64_MAX);
