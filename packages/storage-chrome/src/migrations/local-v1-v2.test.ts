99f542fd1b50d2c1711a2d50827f9916adb5fc2a
turbocrime <134443988+turbocrime@users.noreply.github.com>
2025-07-17 16:27:14 -0700

    improve tests

--- packages/storage-chrome/src/migrations/local-v1-v2.test.ts
+++ packages/storage-chrome/src/migrations/local-v1-v2.test.ts
@@ -1,10 +1,11 @@
 import { Key, KeyPrint } from '@penumbra-zone/crypto-web/encryption';
 import { generateSeedPhrase } from '@penumbra-zone/crypto-web/mnemonic';
 import { AppParameters } from '@penumbra-zone/protobuf/penumbra/core/app/v1/app_pb';
+import { AssetId } from '@penumbra-zone/protobuf/penumbra/core/asset/v1/asset_pb';
 import { Wallet } from '@penumbra-zone/types/wallet';
 import { generateSpendKey, getFullViewingKey, getWalletId } from '@penumbra-zone/wasm/keys';
 import { MockStorageArea } from '@repo/mock-chrome/mocks/storage-area';
-import { beforeEach, describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
 import { ExtensionStorage, ExtensionStorageDefaults } from '../base';
 import { VERSION_FIELD } from '../version-field';
 import * as Storage_V1 from '../versions/v1';
@@ -21,10 +22,21 @@ const testWallet = new Wallet('Test Wallet', testId.toJsonString(), testFvk.toJs
   encryptedSeedPhrase: await testKey.key.seal(testSeedPhrase),
 });
 
-const requiredData: ExtensionStorageDefaults<Storage_V1.LOCAL> = {
+const defaultData: ExtensionStorageDefaults<Storage_V1.LOCAL> = {
   wallets: [],
   knownSites: [],
   numeraires: [],
+};
+
+const validRequiredData: Storage_V1.LOCAL = {
+  wallets: [],
+  knownSites: [{ origin: 'https://example.com', choice: 'Approved', date: 1312 }],
+  numeraires: [new AssetId({ inner: new Uint8Array(1) }).toJsonString()],
+};
+
+const onboardedData: Pick<Storage_V1.LOCAL, 'wallets' | 'passwordKeyPrint'> = {
+  wallets: [testWallet.toJson()],
+  passwordKeyPrint: testKey.keyPrint.toJson(),
 };
 
 const validOptionalData: Omit<Storage_V1.LOCAL, 'wallets' | 'knownSites' | 'numeraires'> = {
@@ -34,12 +46,13 @@ const validOptionalData: Omit<Storage_V1.LOCAL, 'wallets' | 'knownSites' | 'nume
   params: new AppParameters({ chainId: 'mock-chain' }).toJsonString(),
 };
 
-const invalidOptionalData = {
+const invalidOptionalData: Omit<Storage_V1.LOCAL, 'wallets' | 'knownSites' | 'numeraires'> = {
   grpcEndpoint: 123,
   frontendUrl: true,
   fullSyncHeight: 'not-a-number',
   params: { not: 'a-string' },
-};
+  walletCreationBlockHeight: null,
+} as never;
 
 const asLegacyItem = <T>(
   value?: T,
@@ -47,133 +60,215 @@ const asLegacyItem = <T>(
 ): { version: 'V1' | 'V2'; value?: T } => ({ version, value });
 
 const storageArea = new MockStorageArea();
+let v2ExtStorage: ExtensionStorage<Storage_V2.LOCAL, Storage_V2.VERSION>;
 
-describe('v2 local schema migrations', () => {
-  let v2ExtStorage: ExtensionStorage<Storage_V2.LOCAL, Storage_V2.VERSION>;
+describe('local-v1-v2 migration', () => {
+  beforeEach(async () => {
+    await storageArea.clear();
 
-  beforeEach(() => {
-    storageArea.mock.clear();
     v2ExtStorage = new ExtensionStorage<Storage_V2.LOCAL, Storage_V2.VERSION>(
       storageArea,
-      requiredData,
+      defaultData,
       2,
       { 0: local_v0_v1, 1: local_v1_v2 },
     );
   });
 
-  test('passes through valid v1 data unchanged', async () => {
-    const validV1Data: Storage_V1.LOCAL = { ...requiredData, ...validOptionalData };
-    await storageArea.set({ [VERSION_FIELD]: 1, ...validV1Data });
+  afterEach(() => expect(storageArea.mock.get(VERSION_FIELD)).toBe(2));
 
-    const wallets = await v2ExtStorage.get('wallets');
-    expect(wallets).toEqual(validV1Data.wallets);
+  describe('required data is unchanged', () => {
+    test('valid required data is unchanged', async () => {
+      await storageArea.set({
+        [VERSION_FIELD]: 1,
+        ...validRequiredData,
+      });
 
-    const knownSites = await v2ExtStorage.get('knownSites');
-    expect(knownSites).toEqual(validV1Data.knownSites);
+      const wallets = await v2ExtStorage.get('wallets');
+      expect(wallets).toEqual(validRequiredData.wallets);
 
-    const numeraires = await v2ExtStorage.get('numeraires');
-    expect(numeraires).toEqual(validV1Data.numeraires);
+      const knownSites = await v2ExtStorage.get('knownSites');
+      expect(knownSites).toEqual(validRequiredData.knownSites);
 
-    const grpcEndpoint = await v2ExtStorage.get('grpcEndpoint');
-    expect(grpcEndpoint).toEqual(validV1Data.grpcEndpoint);
-
-    const frontendUrl = await v2ExtStorage.get('frontendUrl');
-    expect(frontendUrl).toEqual(validV1Data.frontendUrl);
-
-    const fullSyncHeight = await v2ExtStorage.get('fullSyncHeight');
-    expect(fullSyncHeight).toEqual(validV1Data.fullSyncHeight);
-
-    const params = await v2ExtStorage.get('params');
-    expect(params).toEqual(validV1Data.params);
-
-    expect(await storageArea.get(VERSION_FIELD)).toEqual({ [VERSION_FIELD]: 2 });
-  });
-
-  test('handles invalid optional data (except passwordKeyPrint) by dropping it', async () => {
-    const badData = {
-      ...requiredData,
-      ...validOptionalData,
-      ...invalidOptionalData,
-    };
-
-    await storageArea.set({ [VERSION_FIELD]: 1, ...badData });
-
-    const wallets = await v2ExtStorage.get('wallets');
-    expect(wallets).toEqual(badData.wallets);
-
-    const knownSites = await v2ExtStorage.get('knownSites');
-    expect(knownSites).toEqual(badData.knownSites);
-
-    const numeraires = await v2ExtStorage.get('numeraires');
-    expect(numeraires).toEqual(badData.numeraires);
-
-    const grpcEndpoint = await v2ExtStorage.get('grpcEndpoint');
-    expect(grpcEndpoint).toBeUndefined();
-
-    const frontendUrl = await v2ExtStorage.get('frontendUrl');
-    expect(frontendUrl).toBeUndefined();
-
-    const fullSyncHeight = await v2ExtStorage.get('fullSyncHeight');
-    expect(fullSyncHeight).toBeUndefined();
-
-    const params = await v2ExtStorage.get('params');
-    expect(params).toBeUndefined();
-
-    expect(await storageArea.get(VERSION_FIELD)).toEqual({ [VERSION_FIELD]: 2 });
-  });
-
-  test('migrated data unseals to reveal the original seed phrase', async () => {
-    await storageArea.set({
-      [VERSION_FIELD]: 1,
-      ...requiredData,
-      ...validOptionalData,
-      passwordKeyPrint: testKey.keyPrint.toJson(),
-      wallets: [testWallet.toJson()],
+      const numeraires = await v2ExtStorage.get('numeraires');
+      expect(numeraires).toEqual(validRequiredData.numeraires);
     });
 
-    const wallets = await v2ExtStorage.get('wallets');
-    const passwordKeyPrint = await v2ExtStorage.get('passwordKeyPrint');
+    test('corrupted required data is unchanged', async () => {
+      const corruptedRequiredData = {
+        wallets: asLegacyItem(validRequiredData.wallets),
+        knownSites: asLegacyItem(validRequiredData.knownSites),
+        numeraires: asLegacyItem(validRequiredData.numeraires),
+      };
 
-    const migratedKeyPrint = KeyPrint.fromJson(passwordKeyPrint!);
-    const recreatedKey = await Key.recreate(testPassword, migratedKeyPrint);
+      await storageArea.set({
+        [VERSION_FIELD]: 1,
+        ...corruptedRequiredData,
+      });
 
-    expect(wallets[0]).toStrictEqual(testWallet.toJson());
+      const wallets = await v2ExtStorage.get('wallets');
+      expect(wallets).toStrictEqual(corruptedRequiredData.wallets);
 
-    const walletFromJson = Wallet.fromJson(wallets[0]!);
-    const decryptedSeedPhrase = await recreatedKey!.unseal(
-      walletFromJson.custody.encryptedSeedPhrase,
-    );
-    expect(decryptedSeedPhrase).toBe(testSeedPhrase);
+      const knownSites = await v2ExtStorage.get('knownSites');
+      expect(knownSites).toStrictEqual(corruptedRequiredData.knownSites);
 
-    expect(await storageArea.get(VERSION_FIELD)).toEqual({ [VERSION_FIELD]: 2 });
-  });
-
-  test('migrated data with vestigial passwordKeyPrint unseals to reveal the original seed phrase', async () => {
-    const corruptedData = {
-      ...requiredData,
-      ...validOptionalData,
-      passwordKeyPrint: asLegacyItem(testKey.keyPrint.toJson()),
-      wallets: [testWallet.toJson()],
-    };
-
-    await storageArea.set({
-      [VERSION_FIELD]: 1,
-      ...corruptedData,
+      const numeraires = await v2ExtStorage.get('numeraires');
+      expect(numeraires).toStrictEqual(corruptedRequiredData.numeraires);
     });
 
-    const wallets = await v2ExtStorage.get('wallets');
-    const passwordKeyPrint = await v2ExtStorage.get('passwordKeyPrint');
+    test('invalid required data is unchanged', async () => {
+      await storageArea.set({
+        [VERSION_FIELD]: 1,
+        wallets: 'not-a-wallet',
+        knownSites: 'not-a-known-site',
+        numeraires: 'not-a-numeraire',
+      });
 
-    const migratedKeyPrint = KeyPrint.fromJson(passwordKeyPrint!);
-    const recreatedKey = await Key.recreate(testPassword, migratedKeyPrint);
+      const wallets = await v2ExtStorage.get('wallets');
+      expect(wallets).toBe('not-a-wallet');
 
-    expect(wallets[0]).toStrictEqual(testWallet.toJson());
+      const knownSites = await v2ExtStorage.get('knownSites');
+      expect(knownSites).toBe('not-a-known-site');
 
-    const walletFromJson = Wallet.fromJson(wallets[0]!);
-    const decryptedSeedPhrase = await recreatedKey!.unseal(
-      walletFromJson.custody.encryptedSeedPhrase,
-    );
+      const numeraires = await v2ExtStorage.get('numeraires');
+      expect(numeraires).toBe('not-a-numeraire');
+    });
+  });
 
-    expect(decryptedSeedPhrase).toBe(testSeedPhrase);
+  describe('optional data is unchanged or dropped', () => {
+    test('valid optional data is unchanged', async () => {
+      await storageArea.set({
+        [VERSION_FIELD]: 1,
+        ...validOptionalData,
+      });
+
+      const grpcEndpoint = await v2ExtStorage.get('grpcEndpoint');
+      expect(grpcEndpoint).toBe(validOptionalData.grpcEndpoint);
+
+      const frontendUrl = await v2ExtStorage.get('frontendUrl');
+      expect(frontendUrl).toBe(validOptionalData.frontendUrl);
+
+      const fullSyncHeight = await v2ExtStorage.get('fullSyncHeight');
+      expect(fullSyncHeight).toBe(validOptionalData.fullSyncHeight);
+
+      const params = await v2ExtStorage.get('params');
+      expect(params).toBe(validOptionalData.params);
+
+      const walletCreationBlockHeight = await v2ExtStorage.get('walletCreationBlockHeight');
+      expect(walletCreationBlockHeight).toBeUndefined();
+
+      const compactFrontierBlockHeight = await v2ExtStorage.get('compactFrontierBlockHeight');
+      expect(compactFrontierBlockHeight).toBeUndefined();
+
+      const backupReminderSeen = await v2ExtStorage.get('backupReminderSeen');
+      expect(backupReminderSeen).toBeUndefined();
+    });
+
+    test('corrupted optional data is dropped', async () => {
+      await storageArea.set({
+        [VERSION_FIELD]: 1,
+        grpcEndpoint: asLegacyItem(validOptionalData.grpcEndpoint),
+        frontendUrl: asLegacyItem(validOptionalData.frontendUrl),
+        fullSyncHeight: asLegacyItem(validOptionalData.fullSyncHeight),
+        params: asLegacyItem(validOptionalData.params),
+        walletCreationBlockHeight: asLegacyItem(validOptionalData.walletCreationBlockHeight),
+      });
+
+      const grpcEndpoint = await v2ExtStorage.get('grpcEndpoint');
+      expect(grpcEndpoint).toBeUndefined();
+
+      const frontendUrl = await v2ExtStorage.get('frontendUrl');
+      expect(frontendUrl).toBeUndefined();
+
+      const fullSyncHeight = await v2ExtStorage.get('fullSyncHeight');
+      expect(fullSyncHeight).toBeUndefined();
+
+      const params = await v2ExtStorage.get('params');
+      expect(params).toBeUndefined();
+    });
+
+    test('invalid optional data is dropped', async () => {
+      await storageArea.set({
+        [VERSION_FIELD]: 1,
+        ...invalidOptionalData,
+      });
+
+      const grpcEndpoint = await v2ExtStorage.get('grpcEndpoint');
+      expect(grpcEndpoint).toBeUndefined();
+
+      const frontendUrl = await v2ExtStorage.get('frontendUrl');
+      expect(frontendUrl).toBeUndefined();
+
+      const fullSyncHeight = await v2ExtStorage.get('fullSyncHeight');
+      expect(fullSyncHeight).toBeUndefined();
+
+      const params = await v2ExtStorage.get('params');
+      expect(params).toBeUndefined();
+
+      const walletCreationBlockHeight = await v2ExtStorage.get('walletCreationBlockHeight');
+      expect(walletCreationBlockHeight).toBeUndefined();
+    });
+  });
+
+  describe('handles `passwordKeyPrint` carefully', () => {
+    test('valid passwordKeyPrint is unchanged and valid', async () => {
+      await storageArea.set({
+        [VERSION_FIELD]: 1,
+        ...validOptionalData,
+        ...onboardedData,
+      });
+
+      const wallets = await v2ExtStorage.get('wallets');
+      const passwordKeyPrint = await v2ExtStorage.get('passwordKeyPrint');
+
+      const migratedKeyPrint = KeyPrint.fromJson(passwordKeyPrint!);
+      const recreatedKey = await Key.recreate(testPassword, migratedKeyPrint);
+
+      expect(wallets[0]).toStrictEqual(testWallet.toJson());
+
+      const walletFromJson = Wallet.fromJson(wallets[0]!);
+      const decryptedSeedPhrase = await recreatedKey!.unseal(
+        walletFromJson.custody.encryptedSeedPhrase,
+      );
+      expect(decryptedSeedPhrase).toBe(testSeedPhrase);
+    });
+
+    test('corrupted passwordKeyPrint is recovered and valid', async () => {
+      await storageArea.set({
+        [VERSION_FIELD]: 1,
+        ...validOptionalData,
+        ...onboardedData,
+        passwordKeyPrint: asLegacyItem(onboardedData.passwordKeyPrint),
+      });
+
+      const wallets = await v2ExtStorage.get('wallets');
+      const passwordKeyPrint = await v2ExtStorage.get('passwordKeyPrint');
+
+      const migratedKeyPrint = KeyPrint.fromJson(passwordKeyPrint!);
+      const recreatedKey = await Key.recreate(testPassword, migratedKeyPrint);
+
+      expect(wallets[0]).toStrictEqual(testWallet.toJson());
+
+      const walletFromJson = Wallet.fromJson(wallets[0]!);
+      const decryptedSeedPhrase = await recreatedKey!.unseal(
+        walletFromJson.custody.encryptedSeedPhrase,
+      );
+
+      expect(decryptedSeedPhrase).toBe(testSeedPhrase);
+    });
+
+    test('invalid passwordKeyPrint is unchanged and invalid', async () => {
+      await storageArea.set({
+        [VERSION_FIELD]: 1,
+        ...validOptionalData,
+        ...onboardedData,
+        passwordKeyPrint: 'not-a-key-print',
+      });
+
+      const passwordKeyPrint = await v2ExtStorage.get('passwordKeyPrint');
+
+      // not modified
+      expect(passwordKeyPrint).toBe('not-a-key-print');
+      expect(() => KeyPrint.fromJson(passwordKeyPrint!)).toThrow();
+    });
   });
 });
